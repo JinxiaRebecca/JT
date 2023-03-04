6a03caf890df25eca190bb432699891ced53adfa
JinxiaRebecca <18502908857@163.com>
2023-03-04 15:04:54 +0800

    init goods module

---
+++ dingdang-goods/src/main/java/com/dingdang/DingdangGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.dingdang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingdangGoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DingdangGoodsApplication.class, args);
+	}
+
+}
---
+++ dingdang-goods/src/test/java/com/dingdang/DingdangGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingdang;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingdangGoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
