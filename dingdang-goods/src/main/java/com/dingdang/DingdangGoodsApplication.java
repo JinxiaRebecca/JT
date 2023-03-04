9f3d533048e24ea90b5dc23edc57b6785cc6e929
JinxiaRebecca <18502908857@163.com>
2023-03-04 20:18:10 +0800

    Add common project and nacos discovery config

--- dingdang-goods/src/main/java/com/dingdang/DingdangGoodsApplication.java
+++ dingdang-goods/src/main/java/com/dingdang/DingdangGoodsApplication.java
@@ -1,9 +1,13 @@
 package com.dingdang;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@MapperScan("src/main/java/com/dingdang/product/mapper")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DingdangGoodsApplication {
 
 	public static void main(String[] args) {
